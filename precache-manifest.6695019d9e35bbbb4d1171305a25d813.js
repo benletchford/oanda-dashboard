52a49fe1bebe07f4389672d8d295aca5abe5a57e
Ben Letchford <contact@benl.com.au>
2019-01-05 22:12:24 +1100

    Updates

---
+++ precache-manifest.6695019d9e35bbbb4d1171305a25d813.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21b36dd45983372b1d5e",
+    "url": "/static/css/main.a023eb60.chunk.css"
+  },
+  {
+    "revision": "21b36dd45983372b1d5e",
+    "url": "/static/js/main.21b36dd4.chunk.js"
+  },
+  {
+    "revision": "345356d2b1f114aafb1f",
+    "url": "/static/js/1.345356d2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "166cbce2ffa0a2a2102bfc1e12ec7e1b",
+    "url": "/index.html"
+  }
+];
